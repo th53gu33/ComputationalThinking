6898bee654261147bef7f1484790e666acd1ece9
Th53gu33 <th53gu33@isdedu.de>
2015-09-03 15:34:17 +0200

    Code

--- VowelConsanant.py
+++ VowelConsanant.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+userInput = input("Enter your sentence: ")
+
+vowels = "AEIOUaeiou"
+
+displayVowels = ""
+displayConstanants = ""
+
+for letter in userInput:
+    if letter in vowels:
+        displayVowels += letter
+    else:
+        displayConsanants += letter
+
+print("Vowels: " + displayVowels)
+print("Consonants: " + displayConsonants)
