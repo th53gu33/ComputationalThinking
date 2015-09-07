a7d6a02a08884f419c97c065018e83640b9e39d5
Th53gu33 <th53gu33@isdedu.de>
2015-09-07 13:47:24 +0200

    Code

--- VowelConsanant.py
+++ VowelConsanant.py
@@ -1,16 +1,22 @@
 userInput = input("Enter your sentence: ")
 
 vowels = "AEIOUaeiou"
-consanants = "bcdfghjklmnpqrstvwxyx"
-consanants += consanants.upper()
+consonants = "bcdfghjklmnpqrstvwxyx"
+consonants += consonants.upper()
 
-fvowels = ""
-fconstanants = ""
+displayVowels = ""
+displayConsonants = ""
+displayPunctuation = ""
 
 for letter in userInput:
-    if letter in vowels and consanants:
+    if letter in vowels:
         displayVowels += letter
-        displayConsanants += letter
+    elif letter in consonants:
+        displayConsonants += letter
+    else:
+        displayPunctuation += letter
+
 
 print("Vowels: " + displayVowels)
 print("Consonants: " + displayConsonants)
+print("Punctuation: " + displayPunctuationH)
