8915928d99d18525d314133ea159f76120e82158
Th53gu33 <th53gu33@isdedu.de>
2015-09-09 14:06:31 +0200

    Code

---
+++ ReverseCypher.py
@@ -0,0 +1,9 @@
+userInput = input("Enter your message: ").upper()
+translated = ""
+i = len(userInput)-1
+
+while i >= 0:
+    if message[i] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        translate += message[i]
+    i -= 1
+print(translated)
